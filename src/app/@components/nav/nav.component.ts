5f72ea2fa2620a1bb03746b23aca3d25576d416a
ppetar33 <ppetar33@gmail.com>
2022-12-06 21:22:57 +0100

    Navigation implemented

--- src/app/@components/nav/nav.component.ts
+++ src/app/@components/nav/nav.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-nav',
+  selector: 'navigation',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss']
 })
 export class NavComponent implements OnInit {
+  
+  constructor(
+    private el: ElementRef, 
+  ) { }
 
-  constructor() { }
+  public ngOnInit(): void { }
 
-  ngOnInit(): void {
+  public navigation(event: any): void {
+    event.preventDefault();
+    let box = this.el.nativeElement.querySelectorAll(".box");
+    box.forEach((element: any) => { element.classList.remove('active'); });
+    if (event.path[2].classList[0] === 'box') {
+      event.path[2].classList.add('active');
+    } else {
+      event.path[1].classList.add('active');
+    }
   }
-
 }
