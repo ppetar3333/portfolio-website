368818d29878d06303eab46730d4852a62ace739
ppetar33 <ppetar33@gmail.com>
2022-12-06 23:09:59 +0100

    Header implemented

--- src/app/@pages/start/start.component.ts
+++ src/app/@pages/start/start.component.ts
@@ -2,31 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-start',
+  selector: 'start-page',
   templateUrl: './start.component.html',
   styleUrls: ['./start.component.scss']
 })
 export class StartComponent implements OnInit {
-  public isProgressing: boolean = false;
-
-  constructor(
-    private router: Router
-  ) { }
+  constructor() { }
 
   public ngOnInit(): void {
-    this.fadeOut();
-  }
-
-  public fadeOut(): void {
-    setTimeout(() => {
-      this.isProgressing = true;
-      this.redirectUser();
-    }, 1300);
-  }
-
-  public redirectUser(): void {
-    setTimeout(() => {
-      this.router.navigate(['home'])      
-    }, 1301);
   }
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './@pages/about/about.component';
+import { ContactComponent } from './@pages/contact/contact.component';
+import { ExperienceComponent } from './@pages/experience/experience.component';
 import { HomeComponent } from './@pages/home/home.component';
 import { StartComponent } from './@pages/start/start.component';
+import { WorkComponent } from './@pages/work/work.component';
 
 const routes: Routes = [
   {
@@ -13,8 +17,24 @@ const routes: Routes = [
     component: HomeComponent,
   },
   {
+    path: 'about',
+    component: AboutComponent,
+  },
+  {
+    path: 'expirience',
+    component: ExperienceComponent,
+  },
+  {
+    path: 'contact',
+    component: ContactComponent,
+  },
+  {
+    path: 'work',
+    component: WorkComponent,
+  },
+  {
     path: '**',
-    redirectTo: '/home',
+    redirectTo: '',
     pathMatch: 'full'
   }
 ];
